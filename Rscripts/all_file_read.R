63dde49168855aa0032dfc14d295c0e1de844482
therealgadielsepulveda <therealgadielsepulveda@gmail.com>
2025-06-25 00:16:01 -0400

    File reading script + data
    Completed data reading script and reorganized data. Now, river flow data is inside a nested directory.

--- Rscripts/all_file_read.R
+++ Rscripts/all_file_read.R
@@ -1,40 +1,58 @@
+# OBJETIVO
 # Este script lee los archivos de forma directa.
+# REQUISITOS
 # Se requiere instalar los paquetes "readxl", "tidyverse".
 
+# DATOS
 # Este proyecto contempla sólo dos tipos de datos:
 # 1) Datos de caudal, almacenados en archivos Excel.
 # 2) Datos de precipitaciones, almacenados en archivos .csv.
-# Los archivos han sido nombrados cuidadosamente para permitir la detección.
+# Los archivos han sido nombrados y ubicados cuidadosamente para su correcta lectura.
 
 library(readxl)
 library(tidyverse)
 
-ReadRawInfo <- function(path) {
+# Función de lectura de datos crudos
+# Si el archivo es Excel, lee una hoja de un archivo
+# Si el archivo es .csv, lee el archivo completo
+# En ambos casos, entrega un tibble.
+ReadRawInfo <- function(path, gauge) {
   
   if (str_detect(string=path, pattern=".xls")) {
-    data <- read_excel(path=path, sheet=1)
+    data <- read_excel(path=path, sheet=gauge)
   } else {
     if (str_detect(string = path, pattern = ".csv")) {
-      data <- as_tibble(read.csv(file=path))
+      data <- as_tibble(
+        read.csv(file=path, header=TRUE, sep=";")
+      )
     }
   }
   return(data)
 }
-
-# Lectura de datos de precipitaciones
-# Esta etapa es sencilla y directa.
-
-rawdata_precipitation_maquehue <- ReadRawInfo(path=)
   
-
+# Recoge las rutas de los archivos de datos de caudal.
 flow_filepaths <- dir(
-  path="datos", pattern="flow",
+  path="datos/caudal",
   all.files=FALSE, full.names=TRUE)
 
-# Estas son listas donde cada elemento representa toda la infomración asociada a un archivo.
-rawdata_flow <- vector(mode="list",length=3)
-
-#
+# Estas son listas que contendrán los datos de caudal.
+# Cada lista representa una estación hidrométrica.
+# CCJN: Cautín en Cajón
+# CRRC: Cautín en Rari-Ruca
+# BCCN: Blanco en Curacautín
+# Cada elemento representará toda la información de un año.
+rawdata_flow_CCJN <- vector(mode="list", length=0)
+rawdata_flow_CRRC <- vector(mode="list", length=0)
+rawdata_flow_BCCN <- vector(mode="list", length=0)
+  
+# Lectura de datos para cada año.
 for (path in flow_filepaths) {
-  RadRawInfo(path=path, destination=flow_data)
+  year <- str_extract(string=path, pattern = "20..") # El patrón recogerá los años.
+  rawdata_flow_CCJN[[year]] <- ReadRawInfo(path=path, gauge="RIO CAUTIN EN CAJON")
+  rawdata_flow_CRRC[[year]] <- ReadRawInfo(path=path, gauge="RIO CAUTIN EN RARI-RUCA")
+  rawdata_flow_BCCN[[year]] <- ReadRawInfo(path=path, gauge="RIO BLANCO EN CURACAUTIN")
 }
+
+# Lectura de datos de precipitaciones.
+# Esta etapa es sencilla y directa.
+rawdata_precipitation <- ReadRawInfo(path="datos/precipitation_6h_maquehue.csv")
--- Rscripts/flow_date_yearmonDetection.R
+++ Rscripts/flow_date_yearmonDetection.R
@@ -1,1 +1,10 @@
+# OBJETIVO
+#
+# REQUISITOS
+#
+
 # Detección de meses y años
+
+f1 <- function() {}
+
+f2 <- function() {}
