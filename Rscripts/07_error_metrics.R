0c6f251fe1ffdf2679ce592b28cac9fe9871cdf8
therealgadielsepulveda <therealgadielsepulveda@gmail.com>
2025-07-27 03:27:17 -0400

    Application overhaul
    Improved readability of plots, added separate plots for NSE and RMSE to improve comparison, and added tabs for basic explanation of variables and models.

--- Rscripts/07_error_metrics.R
+++ Rscripts/07_error_metrics.R
@@ -2,7 +2,6 @@
 # ARGUMENTOS:
 # - obs: vector numérico de observaciones de una variable
 # - sim: vector numérico de simulaciones de una variable
-# - steps: pasos predictivos, necesario para recortar las observaciones no predichas.
 # RESULTADO:
 # - coeficiente NSE para el modelo.
 NSECalc <- function(obs, sim) {
